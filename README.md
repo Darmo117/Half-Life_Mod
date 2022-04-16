aa5e670d9a294cc3d767ccf438c0eb219c7d49a6
Darmo117 <darmo117@gmail.com>
2022-04-17 01:45:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Half-Life Mod
+
+Adds elements from the original Half-Life games in Minecraft.
+This mod is primarily developped for a reproduction of _Half-Life: Blue Shift_ I’m currently working on.
---
+++ src/main/java/net/darmo_creations/half_life_mod/HalfLife.java
@@ -0,0 +1,31 @@
+package net.darmo_creations.half_life_mod;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("half_life")
+public class HalfLife {
+  private static final Logger LOGGER = LogManager.getLogger();
+
+  public HalfLife() {
+    FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    MinecraftForge.EVENT_BUS.register(this);
+  }
+
+  private void setup(final FMLCommonSetupEvent event) {
+  }
+
+  @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+  public static class RegistryEvents {
+    @SubscribeEvent
+    public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+    }
+  }
+}
